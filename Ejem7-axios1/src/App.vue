f6d82c0b373d73ceb57ef8900473a3be66d19d98
ik_2dw3a <ik_2dw3a@H015110.NET>
2024-11-22 10:33:18 +0100

    ejem7

---
+++ Ejem7-axios1/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>
+    USO SIMPLE DE AXIOS
+  </h1>
+  <h2>Usuarios de jsonplaceholder
+    
+  </h2>
+
+  <ul>
+    <li v-for="usuario in users" :key="users.id">
+      {{ usuario.name }}
+    </li>
+  </ul>
+
+</template>
+
+<script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+const users = ref([])
+
+onMounted(() => {
+  cargarUsers();
+})
+
+const cargarUsers = async () => {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    console.log(response);
+    users.value = reponse.data;
+  }
+
+  catch (error) {
+    console.error('Error:,', error)
+  }
+}
+</script>
